5914d462bf60cc407dc5e8383bdf76ca43add505
Erick <fachi1997@gmail.com>
2017-08-22 11:19:24 -0300

    verbose_plural

--- rangoapp/models.py
+++ rangoapp/models.py
@@ -6,6 +6,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    class Meta:
+        verbose_name_plural = 'Categories'
     def __str__(self): # For Python 2, use __unicode__ too
         return self.name
 
@@ -15,6 +17,7 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-
+    class Meta:
+        verbose_name_plural = 'Pages'
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
