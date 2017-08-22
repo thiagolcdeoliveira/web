4681a71a1cc8c0ffd0098a5d4e686766001ab66e
Erick <fachi1997@gmail.com>
2017-08-22 11:10:16 -0300

    paginas

--- rangoapp/views.py
+++ rangoapp/views.py
@@ -4,8 +4,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("Hello World! <a href = '/rango/about/'>about</a>")
+#
+# def about(request):
+#     return HttpResponse("Hello World! <a href= '/rango/home/'>home</a> ")
+
 def index(request):
-    return HttpResponse("Hello World! <a href = '/rango/about/'>about</a>")
+    context_dict = {}
+    bold_message = 'E aí, BSI, tudo em cima?'
+    autor = 'Marco André Mendes'
+    context_dict['bold_message'] = bold_message
+    context_dict['autor'] = autor
+
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Hello World! <a href= '/rango/home/'>home</a> ")
+
+    return render(request, 'rango/about.html', {'autor':'Thiago'})
