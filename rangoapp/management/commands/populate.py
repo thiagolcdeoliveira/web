9b3d7d792aa513507da8206d4220527a2750e746
Erick <fachi1997@gmail.com>
2017-10-17 10:54:53 -0200

    populate

--- rangoapp/management/commands/populate.py
+++ rangoapp/management/commands/populate.py
@@ -2,6 +2,7 @@
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rangoapp.models.category import *
 from rangoapp.models.page import *
