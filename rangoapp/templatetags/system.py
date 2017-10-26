0356c1dab445a3814d5f86322710a083298c448a
thiago <thiagolocatellicdeoliveira2gmail.com>
2017-10-26 20:09:50 -0200

    Vader liberou. Curtindo umas verias de verão. Trocando Logo da nave imperial

--- rangoapp/templatetags/system.py
+++ rangoapp/templatetags/system.py
@@ -1,10 +1,14 @@
 from django import template
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.contrib.staticfiles.templatetags.staticfiles import StaticFilesNode
+from django.shortcuts import get_object_or_404
 from django.templatetags.static import StaticNode
 
 from rangoapp.models.category import Category
 from rango.settings import MEDIA_URL
+from rangoapp.models.user_profile import UserProfile
+from rangoapp.views.ranking import calculate_position
+
 register = template.Library()
 
 @register.inclusion_tag('rangoapp/cats.html')
@@ -18,3 +22,10 @@ def media(image=None):
     return MEDIA_URL+str(image)
 
 
+@register.inclusion_tag('includes/user_card_tag.html')
+def user_profile(user=None):
+    context={}
+    context["user_profile"] = get_object_or_404(UserProfile,user=user)
+    context["position"] = calculate_position(context["user_profile"].points)
+    #user_profile["lightning"] = 5
+    return context
