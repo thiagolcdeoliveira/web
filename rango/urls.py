04c816e88527dd7a000333ff16340abbde6aea70
thiago <thiagolocatellicdeoliveira2gmail.com>
2017-11-27 16:56:35 -0200

    caminhos para ataque

--- rango/urls.py
+++ rango/urls.py
@@ -41,15 +41,17 @@ urlpatterns = i18n_patterns(
     url(r'', include('social.apps.django_app.urls', namespace='social')),
     url(r'', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^accounts/', include('registration.backends.default.urls')),
-
+    url(r'^media/(.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^doc/(.*)$', serve, {'document_root': settings.DOC_ROOT}),
     # url(r'^login/$', login, {'template_name': 'registration/login.html'}, name='login'),
     # url(r'^logout/$', login_required(logout), name='logout'),
     # url(r'', include('social.apps.django_app.urls', namespace='social')),
     # url(r'', include('django.contrib.auth.urls', namespace='auth')),
     # url(r'^accounts/', include('registration.backends.default.urls')),
     # url(r'^doc/(.*)$', serve, {'document_root': settings.DOC_ROOT}),
-)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
-              + static(settings.DOC_URL, document_root=settings.DOC_ROOT)
+) \
+    # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+# + static(settings.DOC_URL, document_root=settings.DOC_ROOT)
 # ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #
 # if settings.USE_MODELTRANSLATION:
