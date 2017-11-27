30983bb9b4996c479e1919e8a806dd60b1d8b715
thiago <thiagolocatellicdeoliveira2gmail.com>
2017-11-27 15:01:04 -0200

    refatorando as categorias da nave

--- rango/settings/base.py
+++ rango/settings/base.py
@@ -170,7 +170,7 @@ LOCALE_PATHS = (
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+# STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 DOC_ROOT = os.path.join(PROJECT_DIR, '../../doc')
 DOC_URL = '/doc/'
 #Media files
